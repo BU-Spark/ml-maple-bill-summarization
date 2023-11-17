368f01f3a218caf63a4ed05ca3dbe5db3da37dba
Weining Mai <weining563@gmail.com>
2023-11-17 12:19:15 -0500

    Fix error when click no response

--- demoapp/app.py
+++ demoapp/app.py
@@ -60,8 +60,7 @@ def generate_response(text, title):
     try:
         API_KEY = st.session_state['OPENAI_API_KEY']
     except Exception as e:
-        response = st.error("Invalid [OpenAI API key](https://beta.openai.com/account/api-keys) or not found")
-        return response
+        return st.error("Invalid [OpenAI API key](https://beta.openai.com/account/api-keys) or not found")
 
     # Instantiate LLM model
     with get_openai_callback() as cb:
@@ -71,21 +70,6 @@ def generate_response(text, title):
         
         response = llm.predict(context=text, title=title)
         return response, cb.total_tokens, cb.prompt_tokens, cb.completion_tokens, cb.total_cost
-    
-# Function for Jaccard Score to handle matrix potentially
-# def calculate_jaccard_similarity(text1, text2):
-#     # Tokenize the text into sets of words or tokens
-#     set1 = set(text1.split())
-#     set2 = set(text2.split())
-    
-#     # Convert sets to lists to use with sklearn's jaccard_score
-#     # list1 = list(set1)
-#     # list2 = list(set2)
-    
-#     # Calculates the Jaccard similarity score
-#     jaccard_similarity = jaccard_score(set1, set2)
-#     return jaccard_similarity
-
 
 # Function to update or append to CSV
 def update_csv(title, summarized_bill, csv_file_path):
@@ -115,35 +99,36 @@ with answer_container:
 
     if submit_button:
         
-        # generate response
-        response, total_tokens, prompt_tokens, completion_tokens, total_cost = generate_response(bill_content, bill_title)
-        
-        with col1:
-            st.subheader("Original Bill")
-            st.write(bill_title)
-            st.write(bill_content)
-        
-        with col2:
-            st.subheader("Generated Text")
-            try:
-                update_csv(bill_title, response, csv_file_path)
-                st.write(response)
-                st.download_button(
+        try:
+            response, total_tokens, prompt_tokens, completion_tokens, total_cost = generate_response(bill_content, bill_title)
+            with col1:
+                st.subheader("Original Bill")
+                st.write(bill_title)
+                st.write(bill_content)
+            with col2:
+                st.subheader("Generated Text")
+                try:
+                    update_csv(bill_title, response, csv_file_path)
+                    st.download_button(
                             label="Download Text",
                             data=pd.read_csv("demoapp/generated_bills.csv").to_csv(index=False).encode('utf-8'),
                             file_name='Bills_Summarization.csv',
                             mime='text/csv',)
         
-            except Exception as e:
-                st.error("no response")
+                except Exception as e:
+                    st.error("no response")
+
+            with col3:
+                st.subheader("Evaluation Metrics")
+                st.write(f"Total Tokens: {total_tokens}")
+                st.write(f"Prompt Tokens: {prompt_tokens}")
+                st.write(f"Completion Tokens: {completion_tokens}")
+                st.write(f"Total Cost (USD): ${total_cost}")
+
+        except Exception as e:
+            st.write("no repsonse")
         
-        with col3:
-            st.subheader("Evaluation Metrics")
-            # st.write(f"Jaccard Score: {calculate_jaccard_similarity(bill_content, response)}")
-            st.write(f"Total Tokens: {total_tokens}")
-            st.write(f"Prompt Tokens: {prompt_tokens}")
-            st.write(f"Completion Tokens: {completion_tokens}")
-            st.write(f"Total Cost (USD): ${total_cost}")
+        
                 
 
     
